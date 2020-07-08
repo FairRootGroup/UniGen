2b50f511d57bf3cd9f18e996f28da919547444a6
Dmytro Kresan <d.kresan@gsi.de>
2020-07-08 10:42:19 +0200

    Use script location to determine path to UniGen

--- config/unigenlogin.sh
+++ config/unigenlogin.sh
@@ -3,7 +3,10 @@
 # assuming that this macro is sitting at its original location, 
 # its path can be used to set the unigen enviroment  
 
-export UNIGEN=$PWD
+configpath=$(cd `dirname $0` && pwd && cd $OLDPWD)
+unigenpath=$(dirname $configpath)
+
+export UNIGEN=$unigenpath
 
 export PATH=${UNIGEN}/lib':'${UNIGEN}/bin':'${PATH}
 export LD_LIBRARY_PATH=${UNIGEN}/lib':'${LD_LIBRARY_PATH}
